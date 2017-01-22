c815d7c5c71af5d3e388324a3dcf08f0abbed94f
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2017-01-22 15:29:28 +1100

    Refactor App class to separate initialize from run
    Refactor the App application class to separate its initial
    initialization from its running of its core loop, whilst still providing
    a convenience method to instantiate an instance of itself from a class
    self method `run`.

--- lib/toy_robot/app.rb
+++ lib/toy_robot/app.rb
@@ -1,10 +1,20 @@
 module ToyRobot
   class App
-    def self.run(filename = nil)
-      input = filename ? FileInput.new(filename) : PlayerInput.new
-      Robot.new(input)
+    def self.run(*args)
+      new(*args).run
+    end
 
+    def initialize(filename = nil)
+      @input = filename ? FileInput.new(filename) : PlayerInput.new
+      Robot.new(input)
+    end
+
+    def run
       loop { break if input.update == "EXIT" }
     end
+
+    private
+
+    attr_reader :input
   end
 end
